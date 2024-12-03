5d4781c41431b25bd5b322daca924366b1998156
Lois Flower <lois.flower@liverpool.ac.uk>
2024-12-03 15:47:43 +0000

    Rewrite for day 3 part 2 using regex or and capture groups

--- day3.py
+++ day3.py
@@ -2,19 +2,33 @@
 
 import re
 
-allinstructions = []
-mul = r'mul\([0-9]{1,3},[0-9]{1,3}\)'
-num = r'[0-9]{1,3}'
+# this regex matches the basic "mul\([0-9]{1,3},[0-9]{1,3}\)"
+# but in addition puts the numbers in capture groups with ()
+# and also (using the logical or |) matches the do and don't statements
+# and puts the whole thing in a capture group
+allinst = r"(mul\(([0-9]{1,3}),([0-9]{1,3})\)|do\(\)|don't\(\))"
 
+inputstring = ""
 with open("input3.txt","r") as f:
-  allinstructions = re.findall(mul,f.read())
+  inputstring = f.read()
+
+# findall returns a list of tuples of capture groups
+allinstructions = re.findall(allinst,inputstring)
 
 totalp1 = 0
-
-# test = r'xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))'
-
-for i in range(len(allinstructions)):
-  allinstructions[i] = [ int(n) for n in re.findall(num,allinstructions[i]) ]
-  totalp1 += allinstructions[i][0]*allinstructions[i][1]
+totalp2 = 0
+multp2 = True
+for ins in allinstructions:
+  if "mul" in ins[0]:
+    totalp1 += int(ins[1])*int(ins[2])
+    if multp2:
+      totalp2 += int(ins[1])*int(ins[2])
+  elif ins[0]=="do()":
+    multp2 = True
+  elif ins[0]=="don't()":
+    multp2 = False
+  else:
+    print("Parsing error")
 
 print("Part 1: all multiply instructions gives",totalp1)
+print("Part 2: using do and don'ts, we get",totalp2)
