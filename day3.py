6a95fe4f13b24670380dc6e5e7fef7f9ee2b2a74
Lois Flower <lois.flower@liverpool.ac.uk>
2024-12-03 10:43:24 +0000

    Renaming

--- day3.py
+++ day3.py
@@ -2,19 +2,19 @@
 
 import re
 
-instructions = []
+allinstructions = []
 mul = r'mul\([0-9]{1,3},[0-9]{1,3}\)'
 num = r'[0-9]{1,3}'
 
 with open("input3.txt","r") as f:
-  instructions = re.findall(mul,f.read())
+  allinstructions = re.findall(mul,f.read())
 
-total = 0
+totalp1 = 0
 
 # test = r'xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))'
 
-for i in range(len(instructions)):
-  instructions[i] = [ int(n) for n in re.findall(num,instructions[i]) ]
-  total += instructions[i][0]*instructions[i][1]
+for i in range(len(allinstructions)):
+  allinstructions[i] = [ int(n) for n in re.findall(num,allinstructions[i]) ]
+  totalp1 += allinstructions[i][0]*allinstructions[i][1]
 
-print(total)
+print("Part 1: all multiply instructions gives",totalp1)
