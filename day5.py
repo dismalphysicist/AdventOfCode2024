1280e467424c74ffe385bdae8f7a6016e2fceaca
Lois Flower <lois.flower@durham.ac.uk>
2024-12-05 08:17:24 +0000

    Remove unnecessary code

--- day5.py
+++ day5.py
@@ -1,17 +1,13 @@
 #!/usr/bin/env python3
 
 def passes(update,rule):
-  index0 = -1
-  index1 = -1
-  try:
-    index1 = update.index(rule[1])
-  except ValueError:
-    index1 = -1
   try:
     index0 = update.index(rule[0])
+    index1 = update.index(rule[1])
   except ValueError:
     index0 = -1
-  if index0!=-1 and index1!=-1 and index1<index0:
+    index1 = -1
+  if index0!=-1 and index1<index0:
     return False
   else:
     return True
@@ -28,14 +24,16 @@ def passes_all(update,rules):
 with open("input5.txt","r") as f:
   rules, updates = f.read().strip().split("\n\n")
 
+# process rules
 rules = rules.split("\n")
 for i in range(len(rules)):
   rules[i] = (int(rules[i].split("|")[0]), int(rules[i].split("|")[1]))
-
+# process updates
 updates = updates.split("\n")
 for i in range(len(updates)):
   updates[i] = [ int(n) for n in updates[i].split(",") ]
 
+# loop and add up
 totalp1 = 0
 for update in updates:
   if passes_all(update,rules):
